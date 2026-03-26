63cc95af43e9655942fbda59fe4cac1cab00540d
Shiro-Momon <nazirzato.a@gmail.com>
2026-03-26 16:38:46 +0100

    feat(data): adding the base model

---
+++ src/data/model.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Literal
+
+# Les 4 filières autorisées
+FieldType = Literal["informatique", "mathématiques", "physique", "chimie"]
+
+class StudentBase(BaseModel):
+    firstName: str = Field(..., min_length=2, description="Prénom de l'étudiant (min 2 caractères)")
+    lastName: str = Field(..., min_length=2, description="Nom de l'étudiant (min 2 caractères)")
+    email: EmailStr = Field(..., description="Email valide et unique")
+    grade: float = Field(..., ge=0, le=20, description="Note entre 0 et 20")
+    field: FieldType = Field(..., description="Filière d'étude")
+
+class Student(StudentBase):
+    id: int
